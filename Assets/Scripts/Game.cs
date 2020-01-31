898716ec9b89dfe16059b0f65a7b3cb45d4e341d
Matthew Lowe <lowe.matthew@mail.com>
2020-01-31 19:35:39 +0000

    Add day night cycle

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField] private Camera MainCamera;
+    [SerializeField] private Light DirectionalLight;
+    [SerializeField] private Gradient DayNightGradient;
+    [SerializeField] private GameObject LightRotator;
+    [SerializeField] private float DayNightSpeed;
+
+    private float DayNightT = 0.0f;
+
+    void Start()
+    {
+        StartCoroutine(DayNightCycle());
+    }
+
+    private IEnumerator DayNightCycle()
+    {
+        while (true)
+        {
+            Color col = DayNightGradient.Evaluate(DayNightT);
+            RenderSettings.ambientLight = col;
+            DirectionalLight.color = col;
+            MainCamera.backgroundColor = col;
+            DayNightT += DayNightSpeed * Time.deltaTime;
+            LightRotator.transform.Rotate(DayNightSpeed * 360.0f * Time.deltaTime, 0.0f, 0.0f);
+
+            if (DayNightT > 1.0f)
+                DayNightT = 0.0f;
+
+            Debug.Log(DayNightT);
+
+            yield return null;
+        }
+    }
+}
