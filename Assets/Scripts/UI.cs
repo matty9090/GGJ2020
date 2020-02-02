d750018847cf49364d987525ed1942aaf32148f3
Matthew Lowe <lowe.matthew@mail.com>
2020-02-02 00:47:41 +0000

    Colourise meters

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -8,6 +8,7 @@ public class UI : MonoBehaviour
     [SerializeField] private Resources Resources = null;
     [SerializeField] private Transform MeterWater = null;
     [SerializeField] private Transform MeterOxygen = null;
+    [SerializeField] private Gradient OxygenGradient = null;
     [SerializeField] private TMPro.TextMeshProUGUI TxtSoil = null;
     [SerializeField] private TMPro.TextMeshProUGUI TxtDirt = null;
     [SerializeField] private TMPro.TextMeshProUGUI TxtMetal = null;
@@ -22,6 +23,7 @@ public class UI : MonoBehaviour
     {
         float scale = Mathf.Clamp01(Resources.GetMeter(EMeter.Water) / Resources.MaxWater);
         MeterWater.localScale = new Vector3(MeterWater.localScale.x, scale, MeterWater.localScale.z);
+        MeterWater.GetComponent<Image>().color = OxygenGradient.Evaluate(1.0f - scale);
 
         scale = Mathf.Clamp01(Resources.GetMeter(EMeter.Oxygen) / Resources.MaxOxygen);
         MeterOxygen.localScale = new Vector3(MeterOxygen.localScale.x, scale, MeterOxygen.localScale.z);
