ee81441c7c79c0b3c186d9297f5c67a43f9e2805
echow11 <38040094+echow11@users.noreply.github.com>
2020-02-02 15:57:57 +0000

    Initial Story

---
+++ Assets/Scripts/InitialStory.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InitialStory : MonoBehaviour
+{
+    [SerializeField] private List<TMPro.TextMeshProUGUI> Stories = null;
+    [SerializeField] private int Current = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if (Current < Stories.Count - 1)
+            {
+                ++Current;
+                Stories[Current].enabled = true;
+                Stories[Current - 1].enabled = false;
+            }
+            else
+            {
+                SceneManager.LoadScene("Game");
+            }
+        }
+
+    }
+}
